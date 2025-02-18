853df0d31e974165c3b3d137908d436f43b7cfd9
Anton Polieshchuk <anton2020ua@gmail.com>
2025-02-18 02:19:07 -0800

    init

--- auth.middleware.js
+++ auth.middleware.js
@@ -1,13 +1,31 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
+const authMiddleware = function (req, res, next, authType) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.sendStatus(401)
     jwt.verify(token,  'supo-sect-ketyasdzaerfdsd', (err, user) => {
         console.error(err)
-        if (err) return res.sendStatus(401)
+        if (err || (user.authType !== authType && authType !== '*')) return res.sendStatus(401)
         req.user = user
         next()
     })
 };
+
+const clubAuthMiddleware = function (req, res, next) {
+    return authMiddleware(req, res, next, 'Клуб');
+};
+
+const userAuthMiddleware = function (req, res, next) {
+    return authMiddleware(req, res, next, 'Учасник');
+};
+
+const allAuthMiddleware = function (req, res, next) {
+    return authMiddleware(req, res, next, '*');
+};
+
+module.exports = {
+    clubAuthMiddleware,
+    userAuthMiddleware,
+    allAuthMiddleware
+}
--- handler.js
+++ handler.js
@@ -1,9 +1,9 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const bcrypt = require("bcrypt");
-const authMiddleware = require('./auth.middleware');
+const { clubAuthMiddleware, userAuthMiddleware, allAuthMiddleware } = require('./auth.middleware');
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
@@ -72,11 +72,11 @@ app.get("/clubs", async (req, res, next) => {
     }
 });
 
-app.post("/clubs/join", authMiddleware, async (req, res, next) => {
+app.post("/club/join", userAuthMiddleware, async (req, res, next) => {
     try {
         const { db } = await getMongoDataClient();
         const { clubId } = req.body;
-        await db.collection('users').updateOne({ email: req.user.email }, { $addToSet: { clubs: clubId } });
+        await db.collection('users').updateOne({ email: req.user.email }, { $addToSet: { clubs: ObjectId(clubId) } });
         return res.status(200).json({
             data: 'Success',
         });
@@ -93,7 +93,12 @@ app.post("/user", async (req, res, next) => {
         const { db } = await getMongoDataClient();
         const { password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
-        await db.collection('users').insertOne({ ...req.body, password: hashedPassword, active: true });
+        await db.collection('users').insertOne({
+            ...req.body,
+            password: hashedPassword,
+            active: true,
+            clubs: req.body.clubs?.map(clubId => new ObjectId(clubId)) || [],
+        });
         return res.status(200).json({
             data: 'Success',
         });
@@ -122,13 +127,82 @@ app.get("/users", async (req, res, next) => {
     }
 });
 
-app.get("/clubs/users", async (req, res, next) => {
+app.get("/club/users", clubAuthMiddleware, async (req, res, next) => {
     try {
         const { db } = await getMongoDataClient();
-        const users = await db.collection('users').find({ active: true }).project({ name: 1, nickname: 1, email: 1 });
+        const users = await db.collection('users')
+            .find({ clubs: new ObjectId(req.user._id), active: true })
+            .project({ name: 1, nickname: 1, email: 1 });
         return res.status(200).json({
             items: await users.toArray(),
         });
+    } catch (e) {
+        console.error(e?.message)
+    }
+});
+
+app.get("/user/clubs", userAuthMiddleware, async (req, res, next) => {
+    try {
+        const { db } = await getMongoDataClient();
+        const clubs = await db.collection('users').aggregate([
+            { $match: { email: req.user.email, active: true } },
+            { $lookup: { from: 'clubs', localField: 'clubs', foreignField: '_id', as: 'clubs' } },
+            { $project: { 'clubs.email': 1, 'clubs.name': 1, 'clubs.address': 1 } },
+            { $unwind: '$clubs' },
+            { $project: { email: '$clubs.email', name: '$clubs.name', address: '$clubs.address' } },
+        ]);
+        return res.status(200).json({
+            items: await clubs.toArray(),
+        });
+    } catch (e) {
+        console.error(e?.message)
+    }
+});
+
+app.post("/club/rating-period", clubAuthMiddleware, async (req, res, next) => {
+    try {
+        const { db } = await getMongoDataClient();
+        const { name } = req.body;
+        await db.collection('rating_periods')
+            .insertOne({ name, club: new ObjectId(req.user._id) });
+        return res.status(200).json({
+            status: 'Success',
+        });
+    } catch (e) {
+        console.error(e?.message)
+    }
+});
+
+app.get("/club/rating-periods", allAuthMiddleware, async (req, res, next) => {
+    try {
+        const { db } = await getMongoDataClient();
+        const clubs = []
+        if (req.user.authType === 'Клуб') {
+            clubs.push(new ObjectId(req.user._id));
+        } else {
+            const user = await db.collection('users').findOne({ email: req.user.email });
+            clubs.push(...(user.clubs || []).map(clubId => new ObjectId(clubId)));
+        }
+
+        const periods = await db.collection('rating_periods').aggregate([
+            { $match: { club: { $in: clubs } } },
+            { $lookup: { from: 'clubs', localField: 'club', foreignField: '_id', as: 'club' } },
+            { $project: { name: 1, club: { $arrayElemAt: ['$club', 0] } } },
+            { $sort: { 'club._id': -1, _id: -1 } },
+            { $project: { name: 1, club: '$club.name', clubId: '$club._id'} },
+        ]);
+        let items = await periods.toArray();
+        const map = {}
+        items = items.map(item => {
+            const isLatest = map[item.clubId];
+            if (!isLatest) {
+                map[item.clubId] = true;
+                return ({ ...item, active: true });
+            }
+            return ({ ...item, active: false });
+        });
+        console.log(`--->`, items, 'handler.js:204')
+        return res.status(200).json({ items });
     } catch (e) {
         console.error(e?.message)
     }
